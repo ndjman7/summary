80850377e16a6d5a414c94a248b52db49548c61b
Pando <ndjman7@gmail.com>
2016-12-25 23:57:48 +0900

    Study 13/14/15

---
+++ Python/Learn_PYTHON_the_HARD_WAY/13/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from sys import argv
+
+scirpt, first, second, third, fourth = argv
+
+words = raw_input("하고 싶은 말을 쓰세요")
+
+print scirpt +" "+ first + second + third + fourth
+
+print (words)
